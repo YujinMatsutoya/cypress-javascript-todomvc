82827aa1a77608680e4c2351d4f202832037c213
Yujin Matsutoya <yujin.matsutoya@fvffn0bsq05p.home>
2023-09-28 12:58:39 +0100

    added elements and pom methods

--- cypress/integration/todomvc-filtering.spec.js
+++ cypress/integration/todomvc-filtering.spec.js
@@ -1,28 +1,32 @@
 /// <reference types="cypress" />
 
-describe('filtering', () => {
+import { TodoPage } from "../page-objects/todo-page"
+
+describe('filtering actions', () => {
+
+    const todoPage = new TodoPage()
 
     beforeEach(() => {
-        cy.visit('http://todomvc.com/examples/vanillajs')
-        cy.get('.new-todo').type('item 1{enter}')
-        cy.get('.new-todo').type('item 2{enter}')
-        cy.get('.new-todo').type('item 3{enter}')
-
-        cy.get('li:nth-child(2) .toggle').click()
+        todoPage.navigate()
+        todoPage.addTodo('item number 1{enter}')
+        todoPage.addTodo('item number 2{enter}')
+        todoPage.addTodo('item number 3{enter}')
+        
+        todoPage.toggleTodo(2)
     })
 
-    it('should filter active todos', () => {
-        cy.contains('Active').click()
-        cy.get('.todo-list li').should('have.length', 2)
+    it('Verify filtering for active todos', () => {
+        todoPage.filterActiveTodos()
+        todoPage.elements.todoListItem().should('have.length', 2)
     })
 
     it('should filter Completed todos', () => {
-        cy.contains('Completed').click()
-        cy.get('.todo-list li').should('have.length', 1)
+        todoPage.filterCompletedTodos()
+        todoPage.elements.todoListItem().should('have.length', 1)
     })
 
     it('should filter for All todos', () => {
-        cy.contains('All').click()
-        cy.get('.todo-list li').should('have.length', 3)
+        todoPage.elements.allFilter()
+        todoPage.elements.todoListItem().should('have.length', 3)
     })
 })
--- cypress/page-objects/todo-page.js
+++ cypress/page-objects/todo-page.js
@@ -1,15 +1,45 @@
 export class TodoPage {
     
-    
+    elements = {
+        newTodoInput: () => cy.get('.new-todo'),
+        todoList: () => cy.get('ul.todo-list'),
+        todoListItem: () => cy.get('ul.todo-list li'),
+        todoText: (todoIndex) => cy.get('.todo-list li:nth-child(' + (todoIndex + 1) + ') label'),
+        todoToggle: (todoIndex) => cy.get('.todo-list li:nth-child(' + (todoIndex + 1) + ') input'),
+        todoDelete: (todoIndex) => cy.get('.todo-list li:nth-child(' + (todoIndex + 1) + ') .destroy'),
+        allFilter: (todoIndex) => cy.get('.filters li:nth-child(1) a'),
+        activeFilter: (todoIndex) => cy.get('.filters li:nth-child(2) a'),
+        completedFilter: (todoIndex) => cy.get('.filters li:nth-child(3) a'),
+        todoCount: () => cy.get('footer .todo-count strong'),
+        todoClear: () => cy.get('footer .clear-completed') 
+    }
+
     navigate(){
         cy.visit('http://todomvc.com/examples/vanillajs')
     }
 
     addTodo(todoText){
-        cy.get('.new-todo').type(todoText + '{enter}')
+        this.elements.newTodoInput().type(todoText + '{enter}')
     }
 
-    validateTodoText(todoIndex, expectedText){
-        cy.get('.todo-list li:nth-child(' + (todoIndex + 1) + ') label').should('contain.text', expectedText)
+    toggleTodo(todoIndex){
+        this.elements.todoToggle(todoIndex).click()
     }
+
+    clearTodos(){
+        this.elements.todoClear().click()
+    }
+
+    filterActiveTodos(){
+        this.elements.activeFilter().click()
+    }
+
+    filterCompletedTodos(){
+        this.elements.completedFilter().click()
+    }
+
+    filterAllTodos(){
+        this.elements.allFilter().click()
+    }
+
 }
